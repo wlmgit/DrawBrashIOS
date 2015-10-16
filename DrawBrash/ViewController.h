f4cc12cb8b33172f79a5801fc70e2318ef870c38
wlmgit <1621020745@qq.com>
2015-10-16 15:17:05 +0800

    Initdrawbrash

---
+++ DrawBrash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawBrash
+//
+//  Created by Edison on 15/10/16.
+//  Copyright (c) 2015年 Edison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DrawBrash/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DrawBrash
+//
+//  Created by Edison on 15/10/16.
+//  Copyright (c) 2015年 Edison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
